329082a71015ca64b4f78b4b6ba8b70c2f7132a1
unknown <>
2016-07-10 15:05:41 +0800

    init

---
+++ src/include/prep.h
@@ -0,0 +1,26 @@
+
+#ifndef __PREP_H__
+#define __PREP_H__
+
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/nonfree/features2d.hpp"
+#include "opencv/cvaux.h"
+
+#include <objbase.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <io.h>
+#include <iostream>
+#include <fstream>
+#include <assert.h>
+#include <algorithm>
+#include <cstdlib>
+#include <time.h>
+#include <math.h> 
+
+using namespace std;
+using namespace cv;
+
+#endif
+/* endif __PREP_H__ */
